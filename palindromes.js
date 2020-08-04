29a6b097a72cdc2ee181b6f5d555b3d2e856742e
humberto <thekingtony96@gmail.com>
2020-08-03 20:01:48 -0400

    Primer commit /ejercicio no terminado

--- palindromes.js
+++ palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function() {
+const palindromes = function(str = "") {
+   
 
-}
+        let strInver = "";
+        let strLong = str.length;
+        
+       for (let i = 0; i < str.length; i++) {
+           
+        while(strLong >= 0)
+        {
+            strInver = strInver + str.charAt(strLong);
+            strLong--;
+        }
+    }
+    console.log(strInver);
+    
+       if (strInver.toLowerCase === str.toLowerCase) {
+           return true;
+       }else {
+           return false;
+       }
+    }
+    
+    console.log(palindromoFunc("salas"));
+
+
 
 module.exports = palindromes
